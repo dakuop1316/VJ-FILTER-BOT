fe2db47bc62e240771ec90bfa6099152a7ff36b5
dakuop1316 <dakuop0001@gmail.com>
2025-02-20 22:58:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'justfilesharebot')
 API_ID = int(environ.get('API_ID', '26258063'))
 API_HASH = environ.get('API_HASH', 'be0a0e2ecd938bfc5401d35a399deeb7')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7923574334:AAE0TIZ-bT2xoQYTdSmDqbKbSU4pAT8JUQc")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
@@ -21,7 +21,7 @@ PICS = (environ.get('PICS', '')).split()
 
 
 # Admins & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()] # For Multiple Id Use One Space Between Each.
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7537072860 6883721620').split()] # For Multiple Id Use One Space Between Each.
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]  # For Multiple Id Use One Space Between Each.
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
